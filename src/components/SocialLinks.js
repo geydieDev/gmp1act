6355bed36a13ebcd09252e374aa95a53e7f8a42b
geydieDev <141216113+geydieDev@users.noreply.github.com>
2023-08-18 20:47:52 +0800

    first commit

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -4,13 +4,13 @@ import Nav from 'react-bootstrap/Nav';
 const SocialLinks = ()=> {
   return(
     <div className="d-flex gap-2 p-2 fs-3">
-      <Nav.Link href="https://github.com/">
+      <Nav.Link href="https://github.com/geydieDev/">
         <FaGithub />
       </Nav.Link>
-      <Nav.Link href="https://www.facebook.com/">
+      <Nav.Link href="https://www.facebook.com/kajed.maramba99?mibextid=ZbWKwL">
         <FaFacebook />
       </Nav.Link>
-      <Nav.Link href="https://instagram.com/">
+      <Nav.Link href="https://instagram.com/jade_lang_naman?igshid=MzRlODBiNWFlZA==">
         <FaInstagram />
       </Nav.Link>
     </div>
